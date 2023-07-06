fb4629024ca7bd49e6312b0d9460d391868a9feb
Veronica <vveronikanika@gmail.com>
2023-07-06 18:45:33 +0300

    feat(ngRx): add ngRx and Redux tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+import {StoreDevtoolsModule} from '@ngrx/store-devtools'
+import {StoreModule} from '@ngrx/store'
 
-import { AppComponent } from 'src/app/app.component';
-import { AppRoutingModule } from 'src/app/app-routing.module';
-import { AuthModule } from 'src/app/auth/auth.module';
+import {AppComponent} from 'src/app/app.component'
+import {AppRoutingModule} from 'src/app/app-routing.module'
+import {AuthModule} from 'src/app/auth/auth.module'
+import {environment} from 'src/environments/environment'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AuthModule
+    AuthModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
